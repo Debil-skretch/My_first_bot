9df774421ebb88f0e3f794db640870396745b14f
Genius <ivanmiv2603@gmail.com>
2026-02-04 00:21:31 +1000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,69 @@
+from telebot.types import ReactionTypeEmoji
+import random
+import telebot
+from bot_logic import flip_coin, gen_pass
+TOKEN = "8573001103:AAFIRmxFQYi0Tuk7JPG8D1ln0U1mW13STag"
+bot = telebot.TeleBot(TOKEN)
+
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['password'])
+def send_password(message):
+    bot.reply_to(message, gen_pass(10))
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    coin = flip_coin()
+    bot.reply_to(message, f"Монетка выпала так: {coin}")
+
+@bot.message_handler(func=lambda message: True)
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E", "\xF0\x9F\x98\x8D"]  
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+@bot.message_reaction_handler(func=lambda message: True)
+def get_reactions(message):
+    bot.reply_to(message, f"You changed the reaction from {[r.emoji for r in message.old_reaction]} to {[r.emoji for r in message.new_reaction]}")
+
+bot.infinity_polling(allowed_updates=['message', 'message_reaction'])
+
+bot.polling()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
