ef7eb097439f1a20f63174b62dc9d2a6c9fdfeeb
Genius <ivanmiv2603@gmail.com>
2026-02-04 16:37:07 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ from telebot.types import ReactionTypeEmoji
 import random
 import telebot
 from bot_logic import flip_coin, gen_pass
-TOKEN = "Write your TOKEN"
+TOKEN = "Write yiur TOKEN"
 bot = telebot.TeleBot(TOKEN)
 
     
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    bot.reply_to(message, "Привет! Я первый Telegram бот своего создателя. Напиши что-нибудь!")
+   
 @bot.message_handler(commands=['hello'])
 def send_hello(message):
     bot.reply_to(message, "Привет! Как дела?")
@@ -26,6 +27,10 @@ def send_coin(message):
     coin = flip_coin()
     bot.reply_to(message, f"Монетка выпала так: {coin}")
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "/start - нужна для того чтобы начать,/hello - нужна если хочешь поздоровоться с ботом,/bye - нужна если хочешь попрощаться с ботом,password- нужна если хочешь сгенирировать рандомный парольиз 10 символов")
+
 @bot.message_handler(func=lambda message: True)
 def send_reaction(message):
     emo = ["\U0001F525", "\U0001F917", "\U0001F60E", "\xF0\x9F\x98\x8D"]  
@@ -38,33 +43,3 @@ def get_reactions(message):
 bot.infinity_polling(allowed_updates=['message', 'message_reaction'])
 
 bot.polling()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
