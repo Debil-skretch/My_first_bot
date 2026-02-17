f3751775040c464fb88602dfc808a6d5defcdc44
Genius <ivanmiv2603@gmail.com>
2026-02-18 00:27:48 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import random
 import os
 import telebot
 from bot_logic import flip_coin, gen_pass
-TOKEN = "It's mine"
+TOKEN = "8573001103:AAFIRmxFQYi0Tuk7JPG8D1ln0U1mW13STag"
 bot = telebot.TeleBot(TOKEN)
 
     
@@ -57,8 +57,19 @@ def duck(message):
     image_url = get_duck_image_url()
     bot.reply_to(message, image_url)
 
+@bot.message_handler(commands=['Eco'])
+def eco(message):
+    bot.reply_to(message, "Режим помощи в защите окружающей среды активирован")
+
+facts = ["Стекло разлагается более 1000лет!!!!!", "В море в год от мусора погибает более 1м рыб!!!!","В некоторых местах мира на столько грязно что там сложно дышать"]
+random_facts = random.choice(facts)
+
 @bot.message_handler(func=lambda message: True)
 def send_reaction(message):
+    if message.text == "Как можно использовать пластиковые бутылки?":
+        bot.reply_to(message,"Их можно использовать как многоразовую тару , сдать на переработку или в быту ")
+    elif message.text == "Какие есть факты о экологии?":
+         bot.reply_to(message,random_facts)
     emo = ["\U0001F525", "\U0001F917", "\U0001F60E", "\xF0\x9F\x98\x8D"]  
     bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
 
